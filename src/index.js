ccc17a6720e2c3ba912401494dbfba0180c37e7f
manggong <jihwank1995@gmail.com>
2020-08-01 22:12:07 +0900

    Hide API_KEY

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import API_KEY from '../env';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
-const API_KEY = '';
+console.log(API_KEY);
+
+YTSearch({ key: API_KEY, term: 'surfboards' }, function(data) {
+  console.log(data);
+});
 
 const App = () => {
   return (
