062ed6f04fb90b9b7389742e7904b8f2c6fbe961
manggong <jihwank1995@gmail.com>
2020-08-05 22:48:20 +0900

    Use props

---
+++ src/components/video_list.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+const VideoList = props => {
+  //const videos = props.videos;
+  return <ul className="col-md-4 list-group">{props.videos.length}</ul>;
+};
+
+export default VideoList;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import API_KEY from '../env';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 class App extends Component {
   constructor(props) {
@@ -18,6 +19,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
