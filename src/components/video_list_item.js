945dc6892bdd4313ccc9f611641c022ce85684ad
manggong <jihwank1995@gmail.com>
2020-08-06 21:26:30 +0900

    Add function, object in props

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 
-const VideoListItem = ({ video }) => {
+const VideoListItem = ({ video, onVideoSelect }) => {
   const imageUrl = video.snippet.thumbnails.default.url;
   return (
-    <li className="list-group-item">
+    <li onClick={() => onVideoSelect(video)} className="list-group-item">
       <div className="video-list mdeia">
         <div className="media-left">
           <img className="media-object" src={imageUrl} />
